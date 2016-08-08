7447af154b0a7d15cd666121b75339d6415c0d4d
Lee <dragon8github@github,com>
2016-08-08 13:00:56 +0800

    好纠结

---
+++ test/code/abc.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* 这是注释
+* @Controller
+*/
+class abc
+{
+	function __construct()
+	{
+		# code...
+	}
+
+	function index()
+	{
+		echo 'abc';
+	}
+
+	function test()
+	{
+		echo 'abctest';
+	}
+}
--- test/index.php
+++ test/index.php
@@ -4,10 +4,21 @@
  * User: lee
  * Date: 2016-08-07 
  */
- echo "hello world";
+ header("Content-type: text/html; charset=utf-8");
+ require('vars');
+ require('functions');
 
+ //测试专用临时超恶心路由
  $PATH = $_SERVER["PATH_INFO"];
  $controller = explode('/', $PATH)[1];
  $action = explode('/', $PATH)[2];
- echo $controller . $action;
+ require('./code/'.$controller.'.class.php');
+ // $_controller = new $controller();
+ // $_controller->$action();
 
+ $f = new ReflectionClass($controller);
+ $Doc = $f->getDocComment();
+ if(preg_match('/@Controller/', $Doc))
+ {
+ 	echo "控制器";
+ }
